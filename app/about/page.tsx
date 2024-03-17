a8a28e394ce3d91feb663b9fe474277852504c7b
Farid Mahmudov <Razormad666@gmail.com>
2024-03-17 13:34:12 +0300

    init

---
+++ app/about/page.tsx
@@ -0,0 +1,19 @@
+'use client';
+import { FC } from "react";
+
+
+// export const metadata: Metadata = {
+//     title: "About",
+//     description: "Simple blog app",
+// };
+
+const About: FC = () => {
+    return (
+        
+        
+        <h1>Page About</h1>
+        
+    )
+}
+
+export default About
